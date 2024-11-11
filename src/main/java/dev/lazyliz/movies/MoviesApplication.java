133cec70179041250e974b9ce99eaf85a5edf267
elisa <elidelfrate29@gmail.com>
2024-11-11 09:24:00 +0100

    Added Review class, repository, service and controller. added basic db operations

--- src/main/java/dev/lazyliz/movies/MoviesApplication.java
+++ src/main/java/dev/lazyliz/movies/MoviesApplication.java
@@ -1,12 +1,19 @@
 package dev.lazyliz.movies;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-public class MoviesApplication {
+public class MoviesApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("L'applicazione è stata avviata con successo!");
+	}
 }
--- src/main/java/dev/lazyliz/movies/repositories/ReviewRepository.java
+++ src/main/java/dev/lazyliz/movies/repositories/ReviewRepository.java
@@ -1,14 +1,10 @@
 package dev.lazyliz.movies.repositories;
 
-import dev.lazyliz.movies.models.Movie;
+import dev.lazyliz.movies.models.Review;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-
-    Optional<Movie> findMovieByImdbId(String imdbId);
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
